132bf3260e2c8f47d10e836542a2ddaa6843362f
Serhii Kisil <kisilsergej96@gmail.com>
2023-04-24 21:30:17 +0300

    redux add

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { Provider as ReduxProvider } from "react-redux"
+
+import store from "./store"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+  <React.StrictMode>
+    <ReduxProvider store={store}></ReduxProvider>
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
