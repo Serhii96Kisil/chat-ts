69612a0da9a1f27646a15869e5eaa498dd1818fb
Serhii Kisil <kisilsergej96@gmail.com>
2023-04-27 21:32:23 +0300

    added material ui

--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -1,3 +1,10 @@
+import { Container } from "@mui/material"
+import { Header } from "./components/Header"
+
 export function Root() {
-  return null
+  return (
+    <Container>
+      <Header />
+    </Container>
+  )
 }
